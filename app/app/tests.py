4e5064fad201c536664c2b889be765b794e44df7
AlexashaAle <alexasha992@gmail.com>
2022-03-04 21:52:54 +0530

    Ubdate docker

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
+
 from calc import add
 
 
 class CalcTests(TestCase):
 
     def test_add_numbers(self):
-        """test that two numbers are added together"""
+        """Test that values are added together"""
         self.assertEqual(add(3, 8), 11)
+
+
+
+
