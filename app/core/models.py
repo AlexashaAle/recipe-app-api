f227c3ab81fe1a409201bc8d5fe995766fed5095
AlexashaAle <alexasha992@gmail.com>
2022-03-09 11:39:17 +0530

    Checking why test did't pass

--- app/core/models.py
+++ app/core/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
-class UserManager(BaseUserManager):
 
+class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTests(TestCase):
 
+class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating the new user with email succesesful"""
@@ -15,3 +15,11 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalaized(self):
+        """Test the email for a new user is normalized"""
+        email = "FORANYTEST.me@gmail.com"
+        user = get_user_model().objects.create_user(email, "test123")
+
+        self.assertEqual(user.email, email.lower())
+
