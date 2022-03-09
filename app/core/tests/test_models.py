8d89c15e1fe5078c89b60cc6941b9028189d06e1
AlexashaAle <alexasha992@gmail.com>
2022-03-09 11:49:40 +0530

    Check if travis will pass

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating the new user with email succesesful"""
-        email = "foranytests.me@gmail.com"
+        email = "foranytestsme@gmail.com"
         password = "testpass123"
         user = get_user_model().objects.create_user(
             email=email,
@@ -18,7 +18,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalaized(self):
         """Test the email for a new user is normalized"""
-        email = "FORANYTEST.me@gmail.com"
+        email = "foanytestme@GMAil.com"
         user = get_user_model().objects.create_user(email, "test123")
 
         self.assertEqual(user.email, email.lower())
