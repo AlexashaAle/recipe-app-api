288b12be69b830166151043479b3dd78f81f5de7
AlexashaAle <alexasha992@gmail.com>
2022-03-09 09:43:55 +0530

    1. Adding the user models 2. Make test for user

--- app/core/models.py
+++ app/core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
+    PermissionsMixin
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create and saves the new user"""
+        """Creates and saves a new User"""
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
