39903d2b2e75a99d5100a6881b8e90e13f016983
AlexashaAle <alexasha992@gmail.com>
2022-06-12 17:13:53 +0530

    again

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -3,7 +3,7 @@ import tempfile
 # позволяет проверить если файл сужествует в системе
 import os
 # добавляем пилоу позволяет создать тестовое изображение
-from PIL import Image
+
 
 from django.contrib.auth import get_user_model
 
@@ -251,53 +251,3 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(len(tags), 0)
 
 
-class RecipeImageUploadTests(TestCase):
-
-    def setUp(self):
-        # создаем клиен дляотправки запросов
-        self.client = APIClient()
-        # создаем юзера
-        self.user = get_user_model().objects.create_user(
-            "user@test.com",
-            'testpass'
-        )
-        # авторизуемся в системе
-        self.client.force_authenticate(user=self.user)
-        # создаем тестовый рецепт
-        self.recipe = sample_recipe(user=self.user)
-
-    def tearDown(self):
-        # функция выполняемая после основого теста
-        # удаляем рецепт
-        self.recipe.image.delete()
-
-    def test_uploading_image_to_recipe(self):
-        """test uploading image to recipe"""
-        url = image_upload_url(self.recipe.id)
-        # создаем временный файл с именим и расширением jpg
-        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
-            # создаем изображение черногоквардрата 10 на 10 пикслей
-            img = Image.new('RGB', (10, 10))
-            # сохраняем его во временном файле, указываем формат
-            img.save(ntf, format='JPEG')
-            # перемещает "курсор" в начало загружаемых фйалов
-            ntf.seek(0)
-            # multpart опция для загрузки данный х формате отличном от json
-            res = self.client.post(url, {'image': ntf}, format='multipart')
-        # обновляем данные
-        self.recipe.refresh_from_db()
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        # проверяем наличее изображения в бд
-        self.assertIn("image", res.data)
-        # проверяем наличее пути в файловой системе
-        self.assertTrue(os.path.exists(self.recipe.image.path))
-
-    def test_upload_image_bad_request(self):
-        """Test uploading an invalid image"""
-        url = image_upload_url(self.recipe.id)
-        # отправляем запрс со строкой вместо файла
-        res = self.client.post(
-            url, {'image': "something alse"}, format='multipart')
-
-        # проверяем что загрузить не удалось
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
